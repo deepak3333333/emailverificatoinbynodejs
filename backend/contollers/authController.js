ca8933d343dda7d416e40709cfdd64610cf28ddf
deepak3333333 <deepak20wj1a6713@gmail.com>
2025-03-06 12:01:57 +0530

    second commit

--- backend/contollers/authController.js
+++ backend/contollers/authController.js
@@ -26,6 +26,9 @@ async function register(req, res) {
     const token=jwt.sign({id:user._id,email:user.email},process.env.JWT_SECRET,{expiresIn:"7d"})
    return res.cookie("token",token,{
     httpOnly:true,
+    secure:process.env.NODE_ENV==='production',
+            sameSite:process.env.NODE_ENV==='production'?'none':'strict',
+            maxAge:7*24*60*60*1000
    })
     
   }
